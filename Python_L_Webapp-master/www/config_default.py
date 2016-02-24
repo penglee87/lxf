07ad4663ee4fd37aa2ea0a7d2b7c00226fedb601
penglee87 <penglee87@163.com>
2016-02-24 16:18:50 +0800

    201602241630

---
+++ Python_L_Webapp-master/www/config_default.py
@@ -0,0 +1,16 @@
+#默认配置文件
+configs = {
+	'debug' : True,
+	'db' : {
+		'host' : '127.0.0.1',
+		'port' : 3306,
+		'user' : 'www-data',
+		'password' : 'www-data',
+		'db' : 'awesome'
+	},
+	'session' :{
+		'secret' : 'Awesome'
+	}
+
+
+}
